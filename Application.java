9a5d97f7fbba5df88b86ca5e6d1e6395e5a566d0
Axel Jeremy Solano Illesca <axel.jeremy.7@gmail.com>
2016-04-11 01:42:35 -0400

    Added files via upload

---
+++ Application.java
@@ -0,0 +1,97 @@
+package edu.wmich.cs1120.spring16.la6.tts5535.application;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+import edu.wmich.cs1120.spring16.la6.tts5535.lists.ArrayList;
+import edu.wmich.cs1120.spring16.la6.tts5535.lists.IList;
+import edu.wmich.cs1120.spring16.la6.tts5535.stacks.IStack;
+import edu.wmich.cs1120.spring16.la6.tts5535.stacks.Stack;
+
+public class Application implements IApplication {
+	private String str;// to return each sentence in reverse
+	private IList lista;// to save the sentence of the input.bin
+	private IList chara;// used for reversed words 
+	private IStack stack;// to reverse the each words of the sentences
+    
+	/**
+	* Reads the binary file "input.bin" 
+	* returns each line as a string element in an IList
+	* @return an IList containing the string input.
+	*/
+	@Override
+	public IList readInputFile() {
+		lista = new ArrayList();
+		try {
+			FileInputStream fstream1 = new FileInputStream("input.bin");
+			DataInputStream inputFile1 = new DataInputStream(fstream1);
+			boolean endOfFile1 = false;
+			while (!endOfFile1) {
+				try {						
+							lista.add(inputFile1.readUTF());
+				} catch (EOFException k) {
+					endOfFile1 = true;
+				}
+			}	
+			inputFile1.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return lista;
+	}
+	/**
+	* Writes the reversed string to the binary file "output.bin" 
+	* @param output  , This receive the reverse sentences
+	* 
+	*/
+	@Override
+	public void writeOutputFile(IList output) {
+		
+		try{
+			FileOutputStream fstream = new FileOutputStream("output.bin"); 
+			DataOutputStream outputFile =new DataOutputStream(fstream);
+			for (int i = 0; i < output.size(); i++)
+				outputFile.writeUTF(output.get(i));
+			outputFile.close();
+		}catch(IOException e ){
+			System.out.println(e.getMessage());
+		} 
+	}
+    /**
+     * print to the screen the reverse string  and the no reverse string
+     * @param input :the string that is no reverse
+     * @param output :the sting reversed
+     */
+	@Override
+	public void printToScreen(String input, String output) {
+		System.out.println("The reverse of string \"" + input + "\" is \"" + output + "\".");
+	}
+	
+	/**
+	* Reverses the String parameter.
+	* @param s the String to be reversed 
+	* @return a string :the reversed string
+	*/
+	@Override
+	public String reverseString(String s) {
+		str = "";
+		char[] ch = s.toCharArray();// converting into characters
+		chara = new ArrayList();
+		stack = new Stack();
+		for (int i = 0; i < ch.length; i++) {
+			chara.add(ch[i] + "");// make char to string
+			stack.push(chara.get(i));// adding to stack
+		}
+		for (int i = 0; i < chara.size(); i++) {// reversing
+			str = str + stack.peek();
+			stack.pop();
+		}
+		return str;
+	}
+
+}
---
+++ IApplication.java
@@ -0,0 +1,27 @@
+package edu.wmich.cs1120.spring16.la6.tts5535.application;
+
+import edu.wmich.cs1120.spring16.la6.tts5535.lists.*;
+
+public interface IApplication {
+	
+	/**
+	* reads the binary file "input.bin" and returns each line * as an element in an IList
+	* @return an IList containing the input.
+	*/
+	public IList readInputFile();
+	/**
+	* Writes the reversed string to the binary file "output.bin"
+    * @param output the reverse sentence
+	*/
+	public void writeOutputFile(IList output);
+	default public void printToScreen(String input, String output){ 
+		System.out.println("The reverse of string \""+input+ "\" is\""+output+"\".");
+	      }
+	/**
+	* Reverses the String parameter.
+	* @param s the String to be reversed
+    * @return the reversed string
+	*/
+	public String reverseString(String s);
+
+}
